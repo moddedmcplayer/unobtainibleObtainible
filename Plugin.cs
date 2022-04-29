80bd509e2c2582520ca588398bae1f757d588cf0
moddedmcplayer <moddedmcplayer@gmail.com>
2022-04-29 13:46:24 +0200

    bump

--- Plugin.cs
+++ Plugin.cs
@@ -12,7 +12,7 @@ namespace unobtainibleObtainible
         public override string Author { get; } = "moddedmcplayer";
         public override string Name { get; } = "unobtainibleObtainible";
         public override Version Version { get; } = new Version(1, 1, 0);
-        public override Version RequiredExiledVersion { get; } = new Version(5, 1, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(5, 2, 0);
 
         public Harmony harmony;
         public EventHandlers EventHandler;
